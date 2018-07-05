506b8270d7ac9d54cab76dd8a13938664ef05029
Cristhian Juniti Suematsu <suematsu01@gmail.com>
2018-07-05 01:45:37 -0300

    Configure jokes provider Java Library

---
+++ jokeslib/src/main/java/ladsoft/com/jokeslib/RandomJokeProvider.java
@@ -0,0 +1,10 @@
+package ladsoft.com.jokeslib;
+
+public class RandomJokeProvider {
+
+    public RandomJokeProvider() {}
+
+    public String getRandomJoke() {
+        return "This is a randomly provided joke.";
+    }
+}
