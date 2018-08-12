a752905870603ec890543030aaba219133064fb7
Cristhian Juniti Suematsu <suematsu01@gmail.com>
2018-08-11 22:51:14 -0300

    Implement joke task error handling

--- app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -1,22 +1,9 @@
 package com.udacity.gradle.builditbigger;
 
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ProgressBar;
-
-import com.udacity.gradle.builditbigger.task.JokeProviderTask;
-
-
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends MainActivityFragmentBase {
-
-    public JokeProviderTask jokeTask;
-    private ProgressBar progressBar;
 
     public MainActivityFragment() {
     }
