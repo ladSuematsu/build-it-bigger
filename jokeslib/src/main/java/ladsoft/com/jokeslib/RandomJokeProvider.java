d1a46b00ee37867f61ce8da05df61b0c360a89a0
Cristhian Juniti Suematsu <suematsu01@gmail.com>
2018-08-12 00:12:13 -0300

    Provides varied random jokes

--- jokeslib/src/main/java/ladsoft/com/jokeslib/RandomJokeProvider.java
+++ jokeslib/src/main/java/ladsoft/com/jokeslib/RandomJokeProvider.java
@@ -1,12 +1,23 @@
 package ladsoft.com.jokeslib;
 
+import java.util.Random;
+
 import ladsoft.com.jokeslib.entity.Joke;
 
 public class RandomJokeProvider {
 
+    private final Random random = new Random();
+    private final String[] jokes = {
+            "This is a randomly provided joke.",
+            "This is a randomly provided joke 2.0",
+            "This is a randomly provided Hyper Golden Edition Joke"
+    };
+
     public RandomJokeProvider() {}
 
     public Joke getRandomJoke() {
-        return new Joke("This is a randomly provided joke.");
+        int index = random.nextInt(jokes.length);
+
+        return new Joke(jokes[index]);
     }
 }
